bb417995152a6451ec1835ab967c575d08c6591c
moeka-nemoto <moekanemoto31@gmail.com>
2025-09-20 12:58:46 +0900

    chakraUIの導入

---
+++ app/metadata.ts
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+const siteTitle:string = "Switch2Pr";
+
+export const homeMetaData:Metadata = {
+  title: siteTitle,
+  description: "Switch2Prのトップページ",
+};
+
+export const gameDetailMetaData:Metadata = {
+  title: siteTitle + "/ソフト詳細",
+  description: "ソフトの詳細ページ",
+};
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+import { homeMetaData } from "./metadata";
+
+export const metadata = homeMetaData;
 
 export default function Home() {
   return (
