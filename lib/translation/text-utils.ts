aaf02f6cb44ae8677bcedf10e7093a9d43f4a100
1ssei112266 <144079552+1ssei112266@users.noreply.github.com>
2025-10-24 00:17:46 +0900

    Revert "GameOverview の責務分離とクエリ共通化"

--- lib/translation/text-utils.ts
+++
@@ -1,6 +0,0 @@
-const JAPANESE_CHAR_PATTERN = /[\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\u31F0-\u31FF\uFF66-\uFF9D\u3400-\u4DBF\u4E00-\u9FFF]/;
-
-export function isLikelyJapanese(text: string | null | undefined): boolean {
-  if (!text) return false;
-  return JAPANESE_CHAR_PATTERN.test(text);
-}
